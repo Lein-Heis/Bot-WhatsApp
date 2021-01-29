68ce2c88f6336e0568083aaeed5fa9b25035575d
Lein-Heis <78202307+Lein-Heis@users.noreply.github.com>
2021-01-29 16:22:41 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,9 @@
+let handler = async m => m.reply(`
+「 Donasi • Pulsa 」
+• Telkomsel [087811186838]
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
